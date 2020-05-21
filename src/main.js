596f007ba38874c8e6e9b558a7ae857adcbcf2f6
Andrey Frolov <andrei.frolov@maximumtest.ru>
2020-05-21 13:22:10 +0300

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
-  router,
+  vuetify,
+  router
 }).$mount('#app');

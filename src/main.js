fd75e834f41e375df1b6c1ab0c22af3ada44ad84
Andrey Frolov <log1995q@yandex.ru>
2020-10-23 00:16:14 +0300

    Add axios interceptor

--- src/main.js
+++ src/main.js
@@ -4,8 +4,24 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import i18n from './i18n'
 import store from './store'
+import api from './api'
 
 Vue.config.productionTip = false;
+
+Vue.prototype.$api = api;
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response.status === 401) {
+      // TODO: redirect
+    }
+
+    return error;
+  },
+);
 
 new Vue({
   store,

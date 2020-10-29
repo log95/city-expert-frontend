836a50181e4bc320355b09905add3687cac14f88
Andrey Frolov <andrei.frolov@maximumtest.ru>
2020-10-29 18:41:30 +0300

    Add test list for account

--- src/components/Account/Test/Index.vue
+++ src/components/Account/Test/Index.vue
@@ -1,16 +1,7 @@
 <template>
     <div>
-        <v-btn
-                color="success"
-                class="ma-2 white--text"
-                @click="$router.push({name: 'AccountTestCreatePage'})"
-        >
-            <v-icon left dark>mdi-plus</v-icon>
-            Добавить тест
-        </v-btn>
+
     </div>
-
-
 </template>
 
 <script>
--- src/pages/Account/Test/Index.vue
+++ src/pages/Account/Test/Index.vue
@@ -1,5 +1,18 @@
 <template>
-    <AccountTestIndex></AccountTestIndex>
+    <div>
+        <div>
+            <v-btn
+                    color="success"
+                    class="ma-2 white--text"
+                    @click="$router.push({name: 'AccountTestCreatePage'})"
+            >
+                <v-icon left dark>mdi-plus</v-icon>
+                {{ $t('ADD_TEST') }}
+            </v-btn>
+        </div>
+
+        <AccountTestIndex></AccountTestIndex>
+    </div>
 </template>
 
 <script>
